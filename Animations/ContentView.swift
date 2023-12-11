7908f624ff7a14bafce5c081113e5695b1bf7996
Leo Chung <leo.chung97@gmail.com>
2023-12-11 17:17:24 -0500

    Implemente implicit animations

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -1,19 +1,65 @@
 import SwiftUI
 
+// Note: Implicit animations always need to watch a particular value otherwise animations will be triggered for every small change - including rotating the device from portrait to landscape
+// When you create animations, you are actually creating an instance of an Animation struct that has its own set of modifiers -> you can add .delay(int) directly to the modifier
+// When you use attach an animation() as a modifier (i.e., $animationAmount.animation()) -> you don't have to specify which value we're watching for changes as it is attached to the value it should watch
+
 struct ContentView: View {
     @State private var animationAmount = 1.0
     
     var body: some View {
-        Button("Tap Me") {
-            animationAmount += 1
+        print(animationAmount)
+        return VStack {
+            // Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
+            Stepper("Scale amount", value: $animationAmount.animation(
+                .easeInOut(duration: 1)
+                .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+            
+            Spacer()
+            
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(40)
+            .background(.red)
+            .foregroundStyle(.white)
+            .clipShape(.circle)
+            .scaleEffect(animationAmount)
         }
-        .padding(50)
-        .background(.red)
-        .foregroundStyle(.white)
-        .clipShape(.circle)
-        .scaleEffect(animationAmount)
-        .animation(.default, value: animationAmount)
-        .blur(radius: (animationAmount - 1) * 3)
+        
+        // Button("Tap Me") {
+            // animationAmount += 0.5
+        // }
+        // .padding(50)
+        // .background(.red)
+        // .foregroundStyle(.white)
+        // .clipShape(.circle)
+        // .scaleEffect(animationAmount)
+        // .animation(.default, value: animationAmount)
+        // .animation(.linear, value: animationAmount)
+        // .animation(.spring(duration: 1, bounce: 0.9), value: animationAmount)
+        // .overlay(
+        //    Circle()
+        //        .stroke(.red)
+        //        .scaleEffect(animationAmount)
+        //        .opacity(2 - animationAmount)
+        //        .animation(
+        //            .easeOut(duration: 1)
+        //            .repeatForever(autoreverses: false)
+        //            , value: animationAmount
+        //        )
+        // )
+        // .onAppear {
+        //    animationAmount = 2
+        // }
+        // .animation(
+            // .easeInOut(duration: 0.5)
+                // .delay(0.5)
+                // .repeatCount(3, autoreverses: true)
+                // .repeatForever(autoreverses: true)
+            // , value: animationAmount)
+        // .blur(radius: (animationAmount - 1) * 3)
     }
 }
 
