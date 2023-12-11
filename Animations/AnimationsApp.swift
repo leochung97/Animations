41325de321593e2c6ae8f650de3845b847c21b11
Leo Chung <leo.chung97@gmail.com>
2023-12-11 15:00:06 -0500

    Initial commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Leo Chung on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
