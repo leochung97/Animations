57f694747ab6b9dbe0a8973660d44aa9442d4f5d
Leo Chung <leo.chung97@gmail.com>
2023-12-13 14:31:36 -0500

    Add multiple animation modifiers

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -3,21 +3,23 @@ import SwiftUI
 // Note: Implicit animations always need to watch a particular value otherwise animations will be triggered for every small change - including rotating the device from portrait to landscape
 // When you create animations, you are actually creating an instance of an Animation struct that has its own set of modifiers -> you can add .delay(int) directly to the modifier
 // When you use attach an animation() as a modifier (i.e., $animationAmount.animation()) -> you don't have to specify which value we're watching for changes as it is attached to the value it should watch
+// You can apply an animation() modifier to a view in order to have it implicitly animate changes
 
 struct ContentView: View {
     @State private var animationAmount = 0.0
+    @State private var enabled = false
     
     var body: some View {
         Button("Tap Me") {
-            withAnimation(.spring(duration: 1, bounce: 0.5)) {
-                animationAmount += 360
-            }
+            enabled.toggle()
         }
-        .padding(50)
-        .background(.red)
+        .frame(width: 200, height: 200)
+        .background(enabled ? .blue : .red)
+        .animation(nil, value: enabled)
         .foregroundStyle(.white)
-        .clipShape(.circle)
-        .rotation3DEffect(.degrees(animationAmount), axis: (x: 1, y: 0, z: 0))
+        .clipShape(.rect(cornerRadius: enabled ? 60 : 0))
+        .animation(.spring(duration: 1, bounce: 0.6), value: enabled)
+        // .rotation3DEffect(.degrees(animationAmount), axis: (x: 1, y: 0, z: 0))
         
 //        return VStack {
 //            // Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
