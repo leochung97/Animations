22c54459889f32f4e5db110d57f85e1db2132487
Leo Chung <leo.chung97@gmail.com>
2023-12-15 13:11:17 -0500

    Auto-indent

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,7 +8,7 @@ import SwiftUI
 struct CornerRotateModifier: ViewModifier {
     let amount: Double
     let anchor: UnitPoint
-
+    
     func body(content: Content) -> some View {
         content
             .rotationEffect(.degrees(amount), anchor: anchor)
@@ -30,9 +30,7 @@ struct ContentView: View {
     let letters = Array("Hello SwiftUI")
     @State private var enabled = false
     @State private var dragAmount = CGSize.zero
-    
     @State private var isShowingRed = false
-
     var body: some View {
         ZStack {
             Rectangle()
@@ -77,7 +75,7 @@ struct ContentView: View {
                     isShowingRed.toggle()
                 }
             }
-
+            
             if isShowingRed {
                 Rectangle()
                     .fill(.red)
@@ -86,70 +84,70 @@ struct ContentView: View {
             }
         }
     }
-            // .animation(.bouncy, value: dragAmount)
-        
-        //        Button("Tap Me") {
-//            enabled.toggle()
-//        }
-//        .frame(width: 200, height: 200)
-//        .background(enabled ? .blue : .red)
-//        .animation(nil, value: enabled)
-//        .foregroundStyle(.white)
-//        .clipShape(.rect(cornerRadius: enabled ? 60 : 0))
-//        .animation(.spring(duration: 1, bounce: 0.6), value: enabled)
-        // .rotation3DEffect(.degrees(animationAmount), axis: (x: 1, y: 0, z: 0))
-        
-//        return VStack {
-//            // Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
-//            Stepper("Scale amount", value: $animationAmount.animation(
-//                .easeInOut(duration: 1)
-//                .repeatCount(3, autoreverses: true)
-//            ), in: 1...10)
-//            
-//            Spacer()
-//            
-//            Button("Tap Me") {
-//                animationAmount += 1
-//            }
-//            .padding(40)
-//            .background(.red)
-//            .foregroundStyle(.white)
-//            .clipShape(.circle)
-//            .scaleEffect(animationAmount)
-//        }
-        
-        // Button("Tap Me") {
-            // animationAmount += 0.5
-        // }
-        // .padding(50)
-        // .background(.red)
-        // .foregroundStyle(.white)
-        // .clipShape(.circle)
-        // .scaleEffect(animationAmount)
-        // .animation(.default, value: animationAmount)
-        // .animation(.linear, value: animationAmount)
-        // .animation(.spring(duration: 1, bounce: 0.9), value: animationAmount)
-        // .overlay(
-        //    Circle()
-        //        .stroke(.red)
-        //        .scaleEffect(animationAmount)
-        //        .opacity(2 - animationAmount)
-        //        .animation(
-        //            .easeOut(duration: 1)
-        //            .repeatForever(autoreverses: false)
-        //            , value: animationAmount
-        //        )
-        // )
-        // .onAppear {
-        //    animationAmount = 2
-        // }
-        // .animation(
-            // .easeInOut(duration: 0.5)
-                // .delay(0.5)
-                // .repeatCount(3, autoreverses: true)
-                // .repeatForever(autoreverses: true)
-            // , value: animationAmount)
-        // .blur(radius: (animationAmount - 1) * 3)
+    // .animation(.bouncy, value: dragAmount)
+    
+    //        Button("Tap Me") {
+    //            enabled.toggle()
+    //        }
+    //        .frame(width: 200, height: 200)
+    //        .background(enabled ? .blue : .red)
+    //        .animation(nil, value: enabled)
+    //        .foregroundStyle(.white)
+    //        .clipShape(.rect(cornerRadius: enabled ? 60 : 0))
+    //        .animation(.spring(duration: 1, bounce: 0.6), value: enabled)
+    // .rotation3DEffect(.degrees(animationAmount), axis: (x: 1, y: 0, z: 0))
+    
+    //        return VStack {
+    //            // Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
+    //            Stepper("Scale amount", value: $animationAmount.animation(
+    //                .easeInOut(duration: 1)
+    //                .repeatCount(3, autoreverses: true)
+    //            ), in: 1...10)
+    //
+    //            Spacer()
+    //
+    //            Button("Tap Me") {
+    //                animationAmount += 1
+    //            }
+    //            .padding(40)
+    //            .background(.red)
+    //            .foregroundStyle(.white)
+    //            .clipShape(.circle)
+    //            .scaleEffect(animationAmount)
+    //        }
+    
+    // Button("Tap Me") {
+    // animationAmount += 0.5
+    // }
+    // .padding(50)
+    // .background(.red)
+    // .foregroundStyle(.white)
+    // .clipShape(.circle)
+    // .scaleEffect(animationAmount)
+    // .animation(.default, value: animationAmount)
+    // .animation(.linear, value: animationAmount)
+    // .animation(.spring(duration: 1, bounce: 0.9), value: animationAmount)
+    // .overlay(
+    //    Circle()
+    //        .stroke(.red)
+    //        .scaleEffect(animationAmount)
+    //        .opacity(2 - animationAmount)
+    //        .animation(
+    //            .easeOut(duration: 1)
+    //            .repeatForever(autoreverses: false)
+    //            , value: animationAmount
+    //        )
+    // )
+    // .onAppear {
+    //    animationAmount = 2
+    // }
+    // .animation(
+    // .easeInOut(duration: 0.5)
+    // .delay(0.5)
+    // .repeatCount(3, autoreverses: true)
+    // .repeatForever(autoreverses: true)
+    // , value: animationAmount)
+    // .blur(radius: (animationAmount - 1) * 3)
 }
 
 #Preview {
